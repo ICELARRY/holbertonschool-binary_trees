4a6684b737c0ad0540732c2a2a927ff039987ba4
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-11-15 09:47:10 +0000

    Add binary tree node creation and insert left functions with tests

--- binary_trees.h
+++ binary_trees.h
@@ -2,8 +2,6 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,17 +11,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-};
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
-
+/* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
