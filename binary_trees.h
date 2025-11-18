1358438aca870ffb9fa042bcc02d6e02b6fe569b
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-11-18 06:07:35 +0000

    Add function to delete entire binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -5,7 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Data structure */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
     int n;
@@ -13,12 +20,14 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *); /* Already provided */
 
 #endif /* BINARY_TREES_H */
