6953de20665e9b6b71c408f02d5df50e8335600d
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-11-26 20:17:24 +0000

    Implement binary_tree_postorder: post-order traversal of binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -1,27 +1,28 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/* Binary tree node structure */
-typedef struct binary_tree_s
+/* Structure for a binary tree node */
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
+};
 
-/* Prototypes */
+typedef struct binary_tree_s binary_tree_t;
+
+/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree);
-
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
-
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
