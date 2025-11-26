e5dda8d07c77b553a8e7fed5eb38a5ecf91c7c8d
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-11-26 19:45:44 +0000

    Add prototype for binary_tree_inorder and test file

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,10 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+/* Binary tree node structure */
 typedef struct binary_tree_s
 {
     int n;
@@ -12,11 +13,15 @@ typedef struct binary_tree_s
     struct binary_tree_s *right;
 } binary_tree_t;
 
+/* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_print(const binary_tree_t *tree);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));  /* <== Buraya ekle */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* BINARY_TREES_H */
