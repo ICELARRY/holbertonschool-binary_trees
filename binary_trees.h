ac2c97fea22d0ef3d1ac9d2d5961a70523cd366b
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-11-26 19:31:50 +0000

    Add binary_tree_preorder function and its prototype, with test main file

--- binary_trees.h
+++ binary_trees.h
@@ -4,29 +4,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
-
-/* Function Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_print(const binary_tree_t *); /* Provided by Holberton */
+void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));  /* <== Buraya ekle */
 
 #endif /* BINARY_TREES_H */
