937fe9d9d911bc25b3a0b598630287bf230b55c8
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-11-28 18:53:04 +0000

    Implement binary_tree_size to measure size of binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -2,29 +2,21 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-/* Struct Definition */
-struct binary_tree_s
+/* Struct definition */
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t;
 
-/* Function Prototypes */
+/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-int binary_tree_is_root(const binary_tree_t *node);
-int binary_tree_is_leaf(const binary_tree_t *node);
-size_t binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *);  /* Bunu ekle */
 
 #endif /* BINARY_TREES_H */
