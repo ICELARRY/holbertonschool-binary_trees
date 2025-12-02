42cda5af376dfd9eb12d92bcfcc6af9b76961498
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-12-02 19:16:35 +0000

    Add binary_tree_is_full function and main, update binary_trees.h with prototype

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,21 +12,21 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t;
 
 /* Function prototypes */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *);
-int binary_tree_balance(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
+
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
