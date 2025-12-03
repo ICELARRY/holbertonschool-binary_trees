e247825f69ed7e3543e90b93b04568cbc86f9ce8
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-12-03 10:47:41 +0000

    Add binary_tree_sibling function to find sibling node and update header

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,7 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -12,26 +11,23 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t;
 
 /* Function prototypes */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *);
 
-/* Task-specific functions */
-int binary_tree_balance(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* Other function prototypes */
 
 #endif /* BINARY_TREES_H */
