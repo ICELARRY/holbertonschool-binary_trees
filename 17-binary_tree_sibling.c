799e31db2b660d7e095f25647d35a666aa8e8516
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-12-03 10:49:39 +0000

    Add binary_tree_sibling function to find sibling node and update header

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - Finds the sibling of a node
- * @node: Pointer to the node to find the sibling
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
  *
- * Return: Pointer to the sibling node, or NULL if no sibling
+ * Return: pointer to the sibling node, or NULL if no sibling
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-    if (node->parent->left == node && node->parent->right)
-        return (node->parent->right);
+	if (node->parent->left && node->parent->left != node)
+		return (node->parent->left);
 
-    if (node->parent->right == node && node->parent->left)
-        return (node->parent->left);
+	if (node->parent->right && node->parent->right != node)
+		return (node->parent->right);
 
-    return (NULL);
+	return (NULL);
 }
